5d615c1b23f898fc3667ec22a04aede0c3ae581c
Timothy Shiu <timoweave@gmail.com>
2023-07-06 19:15:36 -0700

    add layout

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # contact
+1. add contact with multiple emails
+1. group emails per person
+1. indicate input error for 2 seconds
+1. disable input if there is an error
+1. able to delete emails
+1. layout the contact like the following:
+   ![contact layout](contact_layout.png)
--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,31 @@ import './App.css';
 
 import { useContact, Contact } from './components/contact';
 
-const exampleContacts: Contact[] = [
+const appContactExamples: Contact[] = [
   {
     name: 'tinker',
     emails: ['tinker@abc.com', 'tinker2@cbc.com', 'tinker3@nbc.com'],
   },
 ];
+
+const appStyleNameAndEmail: React.CSSProperties = {
+  display: 'flex',
+  flexDirection: 'row',
+  gap: '10px',
+};
+
+const appStyleEmail: React.CSSProperties = {
+  display: 'grid',
+  gridTemplateColumns: '1fr 1fr',
+  gap: '60px',
+  padding: '5px',
+};
+
+const appStyleInputs: React.CSSProperties = {
+  display: 'grid',
+  gridTemplateColumns: '1fr 1fr 1fr',
+  gap: '5px',
+};
 
 function App() {
   const {
@@ -20,29 +39,52 @@ function App() {
     deleteEmail,
     onClickInsertNameAndEmail,
   } = useContact({
-    init: exampleContacts,
+    init: appContactExamples,
   });
 
   return (
-    <>
+    <div>
+      <div style={appStyleInputs}>
+        <input
+          disabled={hasError}
+          type="text"
+          value={name}
+          placeholder="name"
+          onChange={onChangeName}
+        ></input>
+        <input
+          disabled={hasError}
+          type="text"
+          value={email}
+          placeholder="email"
+          onChange={onChangeEmail}
+        ></input>
+        <button disabled={hasError} onClick={onClickInsertNameAndEmail}>
+          insert
+        </button>
+        {hasError && <p>has invalid input error</p>}
+      </div>
+      <hr />
       <ul>
         {contacts.map((contact_i: Contact): JSX.Element => {
           const { name, emails } = contact_i;
           return (
             <li key={name}>
-              {name}
+              <div style={appStyleNameAndEmail}>{name}</div>
               <ul>
                 {emails.map(
                   (email_i: string): JSX.Element => (
                     <li key={email_i}>
-                      {email_i}{' '}
-                      <button
-                        onClick={() => {
-                          deleteEmail(name, email_i);
-                        }}
-                      >
-                        delete
-                      </button>
+                      <div style={appStyleEmail}>
+                        {email_i}
+                        <button
+                          onClick={() => {
+                            deleteEmail(name, email_i);
+                          }}
+                        >
+                          delete
+                        </button>
+                      </div>
                     </li>
                   ),
                 )}
@@ -51,26 +93,8 @@ function App() {
           );
         })}
       </ul>
-      <div>
-        <input
-          disabled={hasError}
-          type="text"
-          value={name}
-          onChange={onChangeName}
-        ></input>
-        <input
-          disabled={hasError}
-          type="text"
-          value={email}
-          onChange={onChangeEmail}
-        ></input>
-        <button disabled={hasError} onClick={onClickInsertNameAndEmail}>
-          insert
-        </button>
-        {hasError && <p>has invalid error</p>}
-      </div>
-    </>
+    </div>
   );
-}
+};
 
 export default App;
